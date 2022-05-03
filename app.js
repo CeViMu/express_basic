c66c7eba24ff2b516c38b0e306b5545e94bfdb15
CeViMu <vce02060@innovaccion.mx>
2022-05-02 22:47:06 -0500

    Paso 4: Agregando app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000 //localhost:3000
+
+//esto es sintaxis de express
+app.get('/',(req, res)=>{
+    res.send('Hello World!')
+}) //cuando alguien entre a esta url le aparecera este mensaje
+
+//con esto inicializamos esta app
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`)
+})
