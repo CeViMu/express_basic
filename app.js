b10be985452707408d63d8fedb425b8ded8f83f4
CeViMu <vce02060@innovaccion.mx>
2022-05-04 14:25:22 -0500

    Paso 6: Agregando ruta con parámetros

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ app.get('/launchx',(req, res)=>{
     res.send('Bienvenidos a launchx')
 })//cuando alguien entre a esta url le aparecera este mensaje
 
+//localhost:3000/explorersInNode
+app.get('/explorersInNode',(req, res)=>{
+    const explorer1 = {name: "Explorer1", msg: "Hello"}
+    const explorer2 = {name: "Explorer2", msg: "Hello"}
+    const explorer3 = {name: "Explorer3", msg: "Hello"}
+    res.send([explorer1,explorer2, explorer3])
+})//cuando alguien entre a esta url le aparecera este mensaje
+
+//localhost:3000/explorers/ceciliamuñoz
+app.get('/explorers/:explorerName',(req, res)=>{
+    console.log(req.params) //{explorer: "cecilia"}
+    res.send(req.params)
+})//cuando alguien entre a esta url le aparecera este mensaje
+
 //con esto inicializamos esta app
 app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`)
