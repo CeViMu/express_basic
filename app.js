7bbd95cc8c8b6421212e4849ed72421dd9b92796
CeViMu <vce02060@innovaccion.mx>
2022-05-04 13:06:37 -0500

    Paso 5: Agregando nueva ruta

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const port = 3000 //localhost:3000
 
 //esto es sintaxis de express
 app.get('/',(req, res)=>{
-    res.send('Hello World!')
-}) //cuando alguien entre a esta url le aparecera este mensaje
+     res.send('Hello World!')
+})//cuando alguien entre a esta url le aparecera este mensaje
+
+//localhost:3000/launchx
+app.get('/launchx',(req, res)=>{
+    res.send('Bienvenidos a launchx')
+})//cuando alguien entre a esta url le aparecera este mensaje
 
 //con esto inicializamos esta app
 app.listen(port, ()=>{
